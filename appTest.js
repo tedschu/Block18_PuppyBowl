7e44d490c90063939a334b7f69225b227264a782
tedschu <ted.schuster@gmail.com>
2024-04-29 16:15:56 -0500

    Adding files

---
+++ appTest.js
@@ -0,0 +1,8 @@
+// Lists potential testing requirements:
+
+// Questions / features to test:
+    // All players are displayed on main page
+    // "See details" view displays: (1) player's name, breed, team, (2) enlarged picture, (3) return to main list button, (4) remove button
+        // Does the "remove" button take the player off of the roster?
+    // Does the "add new player" form create a new player?
+        // Does it include (1) player name, (2) breed, (3) submit button
